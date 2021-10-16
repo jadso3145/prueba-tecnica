4f924d16618134a64bcdfb8325139bb28aa805d4
jared salgado <2345jared@gmail.com>
2021-10-16 11:35:13 -0500

    añadiendo materialize

--- js/index.js
+++ js/index.js
@@ -1,19 +1,27 @@
 const botton = document.querySelector("button")
 
+let sliderNum = 5;
+
 document.addEventListener("click", e => {
     if (e.target.matches("button")) {
         const imagen = document.querySelector(".scroll1")
 
+        let distance = imagen.getBoundingClientRect().x - 50
         if (e.target.matches(".left")) {
-            let distance = imagen.getBoundingClientRect().x - 50
-            imagen.style.transform = `translateX(${300 + distance}px)`
-            
+            if (sliderNum > 0) {
+                sliderNum = 6 - 1
+                console.log(sliderNum)
+                imagen.style.transform = `translateX(${380 + distance}px)`
+            }
         }
 
-
+        if (e.target.matches(".rigth")) {
+            if (sliderNumb <= 5) {
+                imagen.style.transform = `translateX(${-380 + distance}px)`
+            }
+        }
 
         console.log(imagen)
         console.log(distance)
     }
 })
-
