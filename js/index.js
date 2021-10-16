4475d9792843ac11bfd0fd51ed12f71497be9f46
jared salgado <2345jared@gmail.com>
2021-10-16 14:21:45 -0500

    agregando slider

--- js/index.js
+++ js/index.js
@@ -1,27 +1,38 @@
-const botton = document.querySelector("button")
+const botton = document.querySelector("img")
 
-let sliderNum = 5;
+let sliderNum = 0
 
 document.addEventListener("click", e => {
-    if (e.target.matches("button")) {
+    if (e.target.matches("img")) {
         const imagen = document.querySelector(".scroll1")
 
         let distance = imagen.getBoundingClientRect().x - 50
         if (e.target.matches(".left")) {
             if (sliderNum > 0) {
-                sliderNum = 6 - 1
-                console.log(sliderNum)
-                imagen.style.transform = `translateX(${380 + distance}px)`
-            }
+                    sliderNum--;
+                    imagen.style.transform = `translateX(${490 + distance}px)`
+                    console.log(sliderNum)
+                }
         }
 
         if (e.target.matches(".rigth")) {
-            if (sliderNumb <= 5) {
-                imagen.style.transform = `translateX(${-380 + distance}px)`
-            }
+            if (sliderNum < 5) {                
+                sliderNum++;
+                console.log(sliderNum)
+                imagen.style.transform = `translateX(${-350 + distance}px)`
+            } 
         }
 
         console.log(imagen)
         console.log(distance)
     }
 })
+
+
+// document.addEventListener("DOMContentLoaded", ()=> {
+//     const elemetCorusel = document.querySelectorAll(".carousel")
+
+//     M.Carousel.init(elemetCorusel, {
+//         duration: 150,
+//     })
+// })
