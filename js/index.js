cfbdc3c885a4e91ea234a783a2510ba661154863
jared salgado <2345jared@gmail.com>
2021-10-16 22:14:04 -0500

    completando el footer

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
 const botton = document.querySelector("img")
 
+const scroll1 = document.querySelector(".scroll1")
+
 let sliderNum = 0
+
+let isDown = false;
+
+let startX;
+
+let scrollLeft;
 
 document.addEventListener("click", e => {
     if (e.target.matches("img")) {
@@ -8,12 +16,12 @@ document.addEventListener("click", e => {
 
         let distance = imagen.getBoundingClientRect().x - 50
         if (e.target.matches(".left")) {
-            if (sliderNum > -1) { 
+            if (sliderNum > -1) {
                 sliderNum--;
                 imagen.style.transform = `translateX(${380 + distance}px)`
                 console.log(sliderNum)
             } else {
-                imagen.style.transform = `translateX(${0}px)`
+                imagen.style.transform = `translateX(${-12}px)`
             }
         }
 
@@ -23,12 +31,39 @@ document.addEventListener("click", e => {
                 console.log(sliderNum)
                 imagen.style.transform = `translateX(${-290 + distance}px)`
             } else {
-                imagen.style.transform = `translateX(${-1900}px)`
+                imagen.style.transform = `translateX(${0}px)`
             }
-            
         }
     }
 })
 
+scroll1.addEventListener("mousedown", (e) => {
+    isDown = true
+    scroll1.classList.add('active')
+    startX = e.pageX - scroll1.offsetLeft;
+    scrollLeft = scroll1.scrollLeft
+    console.log(startX)
+})
 
-console.log(sliderNum)
+scroll1.addEventListener("mouseleave", () => {
+    isDown = false
+    scroll1.classList.remove('active')
+})
+
+scroll1.addEventListener("mouseup", () => {
+    isDown = false
+    scroll1.classList.remove('active')
+})
+
+scroll1.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault()
+    const x = e.pageX - scroll1.offsetLeft
+    const walk = x - startX
+    scroll1.scrollLeft = scrollLeft - walk
+    console.log(walk)
+})
+
+
+
+
