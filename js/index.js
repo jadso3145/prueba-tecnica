7d4799414d6307335b09caf9a7b82438e344f525
jared salgado <2345jared@gmail.com>
2021-10-17 13:26:58 -0500

    agregando animaciones

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const botton = document.querySelector("img")
 
-const scroll1 = document.querySelector(".scroll1")
+const scroll1 = document.querySelector(".scroll1") 
+const animado = document.querySelectorAll('.animado');
 
 let sliderNum = 0
 
@@ -10,32 +11,47 @@ let startX;
 
 let scrollLeft;
 
-document.addEventListener("click", e => {
-    if (e.target.matches("img")) {
-        const imagen = document.querySelector(".scroll1")
 
-        let distance = imagen.getBoundingClientRect().x - 50
-        if (e.target.matches(".left")) {
-            if (sliderNum > -1) {
-                sliderNum--;
-                imagen.style.transform = `translateX(${380 + distance}px)`
-                console.log(sliderNum)
-            } else {
-                imagen.style.transform = `translateX(${-12}px)`
-            }
-        }
 
-        if (e.target.matches(".rigth")) {
-            if (sliderNum < 5) {
-                sliderNum++;
-                console.log(sliderNum)
-                imagen.style.transform = `translateX(${-290 + distance}px)`
-            } else {
-                imagen.style.transform = `translateX(${0}px)`
-            }
-        }
+
+
+window.addEventListener('scroll', ()=> {
+    let  animacion = document.querySelector(".animado")
+    let positionObj1 = animacion.getBoundingClientRect().top;
+    console.log(positionObj1)
+    let pantalla = window.innerHeight/1.5
+    if (positionObj1 < pantalla) {
+        animacion.style.opacity = 1;
+        animacion.style.animation = 'mover 1s ease-out'
     }
-})
+});
+
+// document.addEventListener("click", e => {
+
+//     if (e.target.matches(".flecha")) {
+//         const imagen = document.querySelector(".image3")
+
+//         let distance = imagen.getBoundingClientRect().x - 50
+//         if (e.target.matches(".left")) {
+//             if (sliderNum > 0) {
+//                 sliderNum--;
+//                 imagen.style.transform = `translateX(${380 + distance}px)`
+//                 console.log(sliderNum)
+//             } else {
+//                 // imagen.style.transform = `translateX(${1900}px)`
+//             }
+//         }
+//         if (e.target.matches(".rigth")) {
+//             if (sliderNum < 5) {
+//                 sliderNum++;
+//                 console.log(sliderNum)
+//                 imagen.style.transform = `translateX(${-290 + distance}px)`
+//             } else {
+//                 // imagen.style.transform = `translateX(${0}px)`
+//             }
+//         }
+//     }
+// })
 
 scroll1.addEventListener("mousedown", (e) => {
     isDown = true
@@ -63,6 +79,7 @@ scroll1.addEventListener("mousemove", (e) => {
     scroll1.scrollLeft = scrollLeft - walk
     console.log(walk)
 })
+
 
 
 
